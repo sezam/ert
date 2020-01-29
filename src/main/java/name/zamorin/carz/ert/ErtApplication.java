3628c905abf233e975868531741d92618da0b28e
sezam <sezamo@gmail.com>
2020-01-29 10:39:24 +0500

    first commit

---
+++ src/main/java/name/zamorin/carz/ert/ErtApplication.java
@@ -0,0 +1,13 @@
+package name.zamorin.carz.ert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/name/zamorin/carz/ert/ErtApplicationTests.java
@@ -0,0 +1,13 @@
+package name.zamorin.carz.ert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
